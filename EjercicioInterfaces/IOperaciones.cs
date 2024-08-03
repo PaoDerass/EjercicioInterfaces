460f623e3d4c7657029531fd14179c1ba1480a70
PaoDerass <paola_deras@unitec.edu>
2024-08-02 22:48:55 -0600

    Creacion interfaz

---
+++ EjercicioInterfaces/IOperaciones.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace EjercicioInterfaces
+{
+    internal interface IOperaciones
+    {
+            double AreaRectangulo(double largo, double ancho);
+            double PerimetroRectangulo(double largo, double ancho);
+            double VolumenCubo(double lado);
+            double AreaCubo(double lado);
+            double Circunferencia(double radio);
+    }
+}
